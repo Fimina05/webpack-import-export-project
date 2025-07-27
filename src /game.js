f3d001b57f95b8382b289a3916357d21025a4af9
Fimina05 <115074342+Fimina05@users.noreply.github.com>
2025-07-27 13:07:46 +0300

    game.js

---
+++ src /game.js
@@ -0,0 +1,19 @@
+import Character from './domain.js';
+
+export default class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+  // Заглушка
+}
+
+export function readGameSaving() {
+  // Заглушка
+}
+
+export function writeGameSaving() {
+  // Заглушка
+}
